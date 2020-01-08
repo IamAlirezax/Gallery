e147f01f57993fbcc16aaa355135d275196f20b1
Alirezam <amohammadi84@yahoo.se>
2020-01-08 11:04:52 +0100

    update

--- Gallery/Gallery/ImagePage.xaml.cs
+++ Gallery/Gallery/ImagePage.xaml.cs
@@ -10,9 +10,11 @@ namespace Gallery
         public ImagePage()
         {
             InitializeComponent();
-   var imageSource = new UriImageSource { Uri = new Uri("http://d6jf7ok6u1qis.cloudfront.net/imengine/image.php?uuid=37703dbf-c6cd-5cfe-b8f2-406316533a6a&type=preview&source=false&function=cropresize&width=960&height=480&q=75&x=0.0&y=0.18025&crop_w=1.0&crop_h=0.75&z=1") };
-            imageSource.CachingEnabled = false;
-            image.Source = imageSource;
+            // var imageSource = new UriImageSource { Uri = new Uri("http://www.artlicks.com/files/2379/600px_marjan-jabinpishe.jpg") };
+            //imageSource.CachingEnabled = false;
+            // image.Source = imageSource;
+            btn.Image
+            
             
             
 
